444cb882dbebdc67d3ac7388d6c850a75d932225
asmccormick <asmccormick@gmail.com>
2016-10-31 11:44:38 +0200

    basic gameplay sequence (timer)

---
+++ Sniper/Assets/Code/GameplaySequence.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; // so that I can use Lists
+
+
+public class GameplaySequence : MonoBehaviour {
+
+	public List<GameObject> _firstSpawn;
+	public List<GameObject> _secondSpawn;
+	private bool _firstWaveSpawned;
+	private bool _secondWaveSpawned;
+	private float _startTime;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		SpawnWave(_firstSpawn);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - _startTime > 1 && !_firstWaveSpawned)
+		{
+			SpawnWave(_firstSpawn);
+			_firstWaveSpawned = true;
+		}
+
+		if (Time.time - _startTime > 10 && !_secondWaveSpawned)
+		{
+			SpawnWave(_secondSpawn);
+			_secondWaveSpawned = true;
+		}
+	}
+
+	private void SpawnWave (List<GameObject> _thisWave)
+	{
+		for (int i = 0; i < _thisWave.Count; i ++)
+		{
+			Debug.Log("spawnwave called");
+			_thisWave[i].SetActive(true);
+		}
+	}
+}
