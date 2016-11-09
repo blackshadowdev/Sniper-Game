b4ac70b5fc32ab6dd1358c4c96ffdfa5ef7a006e
asmccormick <asmccormick@gmail.com>
2016-11-09 17:58:53 +0200

    move scope functionality from Rifle.cs to Scope.cs

--- Sniper/Assets/Code/Mert/Rifle.cs
+++ Sniper/Assets/Code/Mert/Rifle.cs
@@ -5,24 +5,16 @@ using UnityEngine.SceneManagement;
 
 
 public class Rifle : MonoBehaviour {
-    [SerializeField] private bool _scopeOn;
     [SerializeField] private bool _rifleCanFire;
-    [SerializeField] private float _rightPadY;
 
     [SerializeField] private AudioSource _rifleAudioSource;
     [SerializeField] private AudioClip _rifleShot;
     [SerializeField] private AudioClip _rifleChamber;
     [SerializeField] private AudioClip _magazineReload;
     
-    public EnemyManager _enemyManagerScript;
-    public Raycaster _raycaster;
+	[SerializeField] private EnemyManager _enemyManagerScript;
+	[SerializeField] private Raycaster _raycaster;
 
-    public Camera _camera;
-    public Camera _scopeCamera;
-    public SpriteRenderer _bulletIcon1;
-    public SpriteRenderer _bulletIcon2;
-    public SpriteRenderer _bulletIcon3;
-    public SpriteRenderer _bulletIcon4;
    
     public bool _fired;
 
@@ -42,7 +34,6 @@ public class Rifle : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-		Debug.Log("currentBullets = " + _currentQuantityBullets);
     }
 
     public void Fire() {
@@ -55,37 +46,6 @@ public class Rifle : MonoBehaviour {
 			_currentQuantityBullets --;
             _raycaster.Raycast();
         }
-    }
-
-    public void ScopeSwitch() {
-        // if Scope View, switch to Normal View
-        if (_scopeOn) {
-            _camera.cullingMask = (1 << 0);
-            _camera.clearFlags = CameraClearFlags.Skybox;
-
-            _scopeOn = false;
-        }
-        // if Normal View, switch to Scope View
-        else if (!_scopeOn) {
-            _camera.cullingMask = (1 << LayerMask.NameToLayer("Scope Image"));
-            _camera.clearFlags = CameraClearFlags.SolidColor;
-            _scopeOn = true;
-        }
-    } // end of ScopeSwitch()
-
-    private void AdjustZoom() {
-        float _tempRightPadY = _rightPadY;
-        if (_tempRightPadY > 0.5f) {
-            _tempRightPadY = 0.5f;
-        }
-        else if (_tempRightPadY < -0.5f) {
-            _tempRightPadY = -0.5f;
-        }
-        _tempRightPadY += 0.5f;
-        float _newFov = _tempRightPadY * 50;
-        _scopeCamera.fieldOfView = _newFov;
-
-        //_scopePivot.LookAt(_head);
     }
 
     public void RoundChambered() {
---
+++ Sniper/Assets/Code/Scope.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scope : MonoBehaviour {
+
+	private bool _scopeOn;
+	[SerializeField] private Camera _camera;
+	[SerializeField] private float _rightPadY;					// this is set by ViveControlsExample, but has been disabled for now in that script.
+	[SerializeField] private Camera _scopeCamera;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//_scopePivot.LookAt(_head);
+	}
+
+	public void ScopeSwitch() {
+		// if Scope View, switch to Normal View
+		if (_scopeOn) {
+			_camera.cullingMask = (1 << 0);
+			_camera.clearFlags = CameraClearFlags.Skybox;
+			_scopeOn = false;
+		}
+		// if Normal View, switch to Scope View
+		else if (!_scopeOn) {
+			_camera.cullingMask = (1 << LayerMask.NameToLayer("Scope Image"));
+			_camera.clearFlags = CameraClearFlags.SolidColor;
+			_scopeOn = true;
+		}
+	}
+
+	private void AdjustZoom() {
+		float _tempRightPadY = _rightPadY;
+		if (_tempRightPadY > 0.5f) {
+			_tempRightPadY = 0.5f;
+		}
+		else if (_tempRightPadY < -0.5f) {
+			_tempRightPadY = -0.5f;
+		}
+		_tempRightPadY += 0.5f;
+		float _newFov = _tempRightPadY * 50;
+		_scopeCamera.fieldOfView = _newFov;
+	}
+}
