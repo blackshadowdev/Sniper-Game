bbf1c171bb4490f75c73c93b4ad587e0111e815a
asmccormick <asmccormick@gmail.com>
2016-11-09 10:31:24 +0200

    early work to separate bullet UI from Rıfle scrıpt

---
+++ Sniper/Assets/Code/BulletUI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletUI : MonoBehaviour {
+
+
+	[SerializeField] private SpriteRenderer _bulletIcon1;
+	[SerializeField] private SpriteRenderer _bulletIcon2;
+	[SerializeField] private SpriteRenderer _bulletIcon3;
+	[SerializeField] private SpriteRenderer _bulletIcon4;
+
+
+	void Start () 
+	{
+	}
+	
+
+	void Update () 
+	{
+	}
+
+
+	public void DecreaseBulletIcons() 
+	{
+		if (_bulletIcon4.enabled) {
+			_bulletIcon4.enabled = false;
+		}
+		else if (_bulletIcon3.enabled) {
+			_bulletIcon3.enabled = false;
+		}
+		else if (_bulletIcon2.enabled) {
+			_bulletIcon2.enabled = false;
+		}
+		else if (_bulletIcon1.enabled) {
+			_bulletIcon1.enabled = false;
+		}
+	}
+
+
+	public void RestoreBulletIcons() 
+	{
+		_bulletIcon1.enabled = true;
+		_bulletIcon2.enabled = true;
+		_bulletIcon3.enabled = true;
+		_bulletIcon4.enabled = true;
+	}
+}
