13d3f967713a34b3caec52036a1876332bce61ed
asmccormick <asmccormick@gmail.com>
2016-11-14 15:57:44 +0200

    circle loader for spotter tool

--- Sniper/Assets/Code/SpotterTool.cs
+++ Sniper/Assets/Code/SpotterTool.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 
 public class SpotterTool : MonoBehaviour {
@@ -10,23 +11,29 @@ public class SpotterTool : MonoBehaviour {
 	[SerializeField] private EnemyManager _enemyManager;
 	[SerializeField] private List<Renderer> _spottedEnemyRenderers;		// if I make this private (non-serialized) it doesn't work.  Why?  There's no need to expose it in the inspector, so I want it to be just private.
 	private float _spotterStartTime;
+	private float _spotterEndTime;
 	private bool _needToTurnOffSpotter;
+	[SerializeField] private Image _spotterUI;
+
+
  
 	// Use this for initialization
 	void Start () {
-	
+		_spotterUI.fillAmount = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		#if UNITY_EDITOR
 		if (Input.GetKeyDown("s"))
 		{
 			ActivateSpotter();
 		}
+		#endif
 
 		if (_needToTurnOffSpotter)
 		{
-			if (Time.time - _spotterStartTime > 5)
+			if (Time.time > _spotterEndTime)
 			{
 				for (int i = 0; i < _spottedEnemyRenderers.Count; i++)
 				{
@@ -35,18 +42,33 @@ public class SpotterTool : MonoBehaviour {
 				_spottedEnemyRenderers.Clear();
 				_needToTurnOffSpotter = false;
 			}
-		}
+		} else if (!_needToTurnOffSpotter && _spotterUI.fillAmount < 1)
+			FillSpotterUI();
 	}
 
 	public void ActivateSpotter ()
 	{
-		_spotterStartTime = Time.time;
-		_needToTurnOffSpotter = true;
-		for (int i = 0; i < _enemyManager._activeEnemies.Count; i++)
+		if (_spotterUI.fillAmount == 1)
 		{
-			Renderer _tempRenderer = _enemyManager._activeEnemies[i].transform.FindChild("RPG-Character-Mesh").GetComponent<Renderer>();
-			_spottedEnemyRenderers.Add(_tempRenderer);
-			_spottedEnemyRenderers[i].material = _spotterMaterial;
+			_spotterUI.fillAmount = 0;
+			_spotterStartTime = Time.time;
+			_spotterEndTime = Time.time + 5;
+			_needToTurnOffSpotter = true;
+			for (int i = 0; i < _enemyManager._activeEnemies.Count; i++)
+			{
+				Renderer _tempRenderer = _enemyManager._activeEnemies[i].transform.FindChild("RPG-Character-Mesh").GetComponent<Renderer>();
+				_spottedEnemyRenderers.Add(_tempRenderer);
+				_spottedEnemyRenderers[i].material = _spotterMaterial;
+			}
 		}
+	}
+
+	public void FillSpotterUI ()
+	{
+		float _elapsedTime = Time.time - _spotterEndTime;
+		float _uiFillAmount = _elapsedTime/30;
+		if (_uiFillAmount > 1)
+			_uiFillAmount = 1;
+		_spotterUI.fillAmount = _uiFillAmount;
 	}
 }
